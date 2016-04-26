bff80bc9620be9932054fadfe85442f785b53389
Francisco Carrera <fc1159@nyu.edu>
2016-04-26 16:12:58 -0400

    - all blocks reset to initial position  - new clear all button reverts to original state  - trampolines are no longer fixed and will drop on the platforms

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+    private Vector3 initialPosition;
+    private Quaternion initialRotation;
+    private Rigidbody2D body;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+        initialPosition = GetComponent<Transform>().position;
+        initialRotation = GetComponent<Transform>().rotation;
+    }
+
+    void GameReset()
+    {
+        body.isKinematic = true;
+        transform.rotation = initialRotation;
+        transform.position = initialPosition;
+        body.isKinematic = false;
+    }
+
+    void OnEnable()
+    {
+        //GameEventManager.GameLaunch += GameLaunch;
+        GameEventManager.GameReset += GameReset;
+    }
+
+    void OnDisable()
+    {
+       // GameEventManager.GameLaunch -= GameLaunch;
+        GameEventManager.GameReset -= GameReset;
+    }
+}
---
+++ Assets/Scripts/GameEventManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class GameEventManager
+{
+
+    public delegate void GameEvent();
+
+    public static event GameEvent GameLaunch, GameReset;
+
+    public static void TriggerGameLaunch()
+    {
+        if (GameLaunch != null)
+        {
+            GameLaunch();
+        }
+    }
+
+    public static void TriggerGameReset()
+    {
+        if (GameReset != null)
+        {
+            GameReset();
+        }
+    }
+}
