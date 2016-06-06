0cfe31289953df0d2e8049503932e1a99c242e64
SKarthikShivaa <KSaiKumar@fullsail.edu>
2016-06-06 09:08:39 -0400

    Sounds and bug fixes

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -18,6 +18,7 @@ public class UIManager : MonoBehaviour {
 
     void Awake()
     {
+        gameObject.GetComponent<Canvas>().worldCamera = Camera.main;
         startBlock = GameObject.FindGameObjectWithTag("StartBlock");
         if (startRight == true)
         {
