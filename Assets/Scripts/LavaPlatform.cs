73278c9e64a4b1d5a7c4f11a458bbf13ffa1702a
Francisco Carrera <fc1159@nyu.edu>
2016-06-18 11:20:52 -0400

    == Platforms ==  - Ice Platforms Are destroyed when hit by a bolt  - Lava Platforms Destroy Objects That Touch Them

---
+++ Assets/Scripts/LavaPlatform.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LavaPlatform : MonoBehaviour {
+
+    public float secondsToDestroy = 3f;
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "Bolt")
+        {
+            Debug.Log("Lava Hit!");
+            Destroy(col.gameObject);
+        }
+        
+        else
+        {
+            StartCoroutine(DestroyObject(col.gameObject));
+        } 
+    }
+
+    IEnumerator DestroyObject(GameObject toDestroy)
+    {
+        yield return new WaitForSeconds(secondsToDestroy);
+        Destroy(toDestroy);
+    }
+}
