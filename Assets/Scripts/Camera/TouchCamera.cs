d5f323816e78de497bbab9e710001953f1685926
Francisco Carrera <fc1159@nyu.edu>
2016-12-20 12:02:05 -0500

    Now TouchCamera.cs has no compiler errors

--- Assets/Scripts/Camera/TouchCamera.cs
+++ Assets/Scripts/Camera/TouchCamera.cs
@@ -3,12 +3,19 @@
 using UnityEngine;
 
 public class TouchCamera : MonoBehaviour {
-	Vector2?[] oldTouchPositions = {
+
+    Vector2?[] oldTouchPositions = {
 		null,
 		null
 	};
 	Vector2 oldTouchVector;
 	float oldTouchDistance;
+    Camera cam;
+
+    void Awake()
+    {
+        cam = GetComponent<Camera>();
+    }
 
 	void Update() {
 		if (Input.touchCount == 0) {
@@ -23,7 +30,7 @@ public class TouchCamera : MonoBehaviour {
 			else {
 				Vector2 newTouchPosition = Input.GetTouch(0).position;
 				
-				transform.position += transform.TransformDirection((Vector3)((oldTouchPositions[0] - newTouchPosition) * camera.orthographicSize / camera.pixelHeight * 2f));
+				transform.position += transform.TransformDirection((Vector3)((oldTouchPositions[0] - newTouchPosition) * cam.orthographicSize / cam.pixelHeight * 2f));
 				
 				oldTouchPositions[0] = newTouchPosition;
 			}
@@ -36,7 +43,7 @@ public class TouchCamera : MonoBehaviour {
 				oldTouchDistance = oldTouchVector.magnitude;
 			}
 			else {
-				Vector2 screen = new Vector2(camera.pixelWidth, camera.pixelHeight);
+				Vector2 screen = new Vector2(cam.pixelWidth, cam.pixelHeight);
 				
 				Vector2[] newTouchPositions = {
 					Input.GetTouch(0).position,
@@ -45,10 +52,10 @@ public class TouchCamera : MonoBehaviour {
 				Vector2 newTouchVector = newTouchPositions[0] - newTouchPositions[1];
 				float newTouchDistance = newTouchVector.magnitude;
 
-				transform.position += transform.TransformDirection((Vector3)((oldTouchPositions[0] + oldTouchPositions[1] - screen) * camera.orthographicSize / screen.y));
+				transform.position += transform.TransformDirection((Vector3)((oldTouchPositions[0] + oldTouchPositions[1] - screen) * cam.orthographicSize / screen.y));
 				transform.localRotation *= Quaternion.Euler(new Vector3(0, 0, Mathf.Asin(Mathf.Clamp((oldTouchVector.y * newTouchVector.x - oldTouchVector.x * newTouchVector.y) / oldTouchDistance / newTouchDistance, -1f, 1f)) / 0.0174532924f));
-				camera.orthographicSize *= oldTouchDistance / newTouchDistance;
-				transform.position -= transform.TransformDirection((newTouchPositions[0] + newTouchPositions[1] - screen) * camera.orthographicSize / screen.y);
+				cam.orthographicSize *= oldTouchDistance / newTouchDistance;
+				transform.position -= transform.TransformDirection((newTouchPositions[0] + newTouchPositions[1] - screen) * cam.orthographicSize / screen.y);
 
 				oldTouchPositions[0] = newTouchPositions[0];
 				oldTouchPositions[1] = newTouchPositions[1];
